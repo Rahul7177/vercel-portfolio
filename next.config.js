fe520a3366b990f0289c1887176e25d953a4e229
Rahul Raj <rahulr7177@gmail.com>
2025-08-12 15:52:18 +0530

    configured deployment

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const createMDX = require('@next/mdx')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: false,
-  },
+
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 };
 
